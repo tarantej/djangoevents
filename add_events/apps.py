a56e2fd810f4f5afdd957a3b3f29389fcbd859ef
Taran Tej <21302579+tarantej@users.noreply.github.com>
2020-03-22 18:26:15 +1300

    Added new app - "Add Events" in project Added url for "Homepage" Added Bootstrap theme package for python
    Project has now 3 links
    
    index
    admin
    add_events

---
+++ add_events/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AddEventsConfig(AppConfig):
+    name = 'add_events'
--- assignment1_7420/settings.py
+++ assignment1_7420/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'add_events.apps.AddEventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
