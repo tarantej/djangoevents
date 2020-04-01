49f33298b5f86c59bd9cbd286466c16c86ce8b5a
Taran Tej <21302579+tarantej@users.noreply.github.com>
2020-04-02 08:17:55 +1300

    Adding Media Root directory in the project and adding events in database via admin panel

--- assignment1_7420/urls.py
+++ assignment1_7420/urls.py
@@ -13,28 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from . import views
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('events/', include('events.urls')),
+                  path('admin/', admin.site.urls),
+                  path('', views.index, name='index'),
+                  path('about/', views.about, name='about'),
+                  path('events/', include('events.urls')),
 
+                  # path('events/', views.events_index, name='events_index'),
+                  # path('events/add_events', views.add_events, name='add_events'),
 
-
-
-
-
-    # path('events/', views.events_index, name='events_index'),
-    # path('events/add_events', views.add_events, name='add_events'),
-
-
-
-
-]
-
-
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import date
+
 
 # Create your models here.
+class EventsList(models.Model):
+    event_name = models.CharField(max_length=200)
+    event_date = models.DateField(auto_now=False, auto_now_add=False)
+    event_location = models.CharField(max_length=200)
+    event_desc = models.TextField(blank=True)
+    featured_image = models.ImageField(upload_to='images/', blank=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.event_name
